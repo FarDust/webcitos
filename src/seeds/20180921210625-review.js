e562e66c9c741167092e564a468f3e14195dde46
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-09-21 19:20:18 -0300

    :file_cabinet: Created reviews seed

--- src/seeds/20180921210625-review.js
+++ src/seeds/20180921210625-review.js
@@ -1,26 +1,24 @@
 'use strict';
+const faker = require('faker');
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+  up: queryInterface => queryInterface
+    .bulkInsert(
+      'reviews',
+      Array(50).fill(undefined).map(() => ({
+        content: faker.lorem.sentence(),
+        quality_offered: getRandomInt(1,5),
+        puntuality: getRandomInt(1,5),
+        fullfillment_offer: getRandomInt(1,5),
+        createdAt: faker.date.past(0.5, new Date(2017, 0, 1)),
+        updatedAt: faker.date.past(0.5, new Date(2018, 0, 1)),
+      })),
+    ),
 
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
-  },
-
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
-  }
+  down: queryInterface => queryInterface.bulkDelete('users', null, {}),
 };
