55ed34ccbf0fbfc1907bf45a2bd81c938cd4c713
jatrijori <jtjorquera@uc.cl>
2018-09-21 19:17:59 -0300

    edit, new, index, delete trades

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ const KoaRouter = require('koa-router');
 const index = require('./routes/index');
 const hello = require('./routes/hello');
 const users = require('./routes/users');
+const trades = require('./routes/trades');
 
 const router = new KoaRouter();
 
 router.use('/', index.routes());
 router.use('/hello', hello.routes());
 router.use('/users', users.routes());
+router.use('/trades', trades.routes());
 
 module.exports = router;
