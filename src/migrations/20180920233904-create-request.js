6976da1c785b87b3c074198cc54f540ecb1ca303
entamburini <entamburini@uc.cl>
2018-09-21 18:37:09 -0300

    all seeds done

--- src/migrations/20180920233904-create-request.js
+++ src/migrations/20180920233904-create-request.js
@@ -11,14 +11,6 @@ module.exports = {
       message: {
         type: Sequelize.STRING
       },
-      userId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      publicationId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
