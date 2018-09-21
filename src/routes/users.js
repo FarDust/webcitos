8beaf6b53492ffe25acd7988a5a97a5fb49198c4
jatrijori <jtjorquera@uc.cl>
2018-09-21 00:15:36 -0300

    create, edit, index users

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,32 +1,62 @@
 const KoaRouter = require('koa-router');
 const router = new KoaRouter();
-router.get('users', '/', async (ctx) => {
-  ctx.body = await ctx.orm.user.findAll();
+
+router.param('id', async (id, ctx, next) => {
+  const user = await ctx.orm.user.findById(ctx.params.id);
+  ctx.assert(user, 404);
+  ctx.state.user = user;
+  return next();
 });
+
+router.get('users', '/', async (ctx) => {
+  const users = await ctx.orm.user.findAll();
+  return ctx.render('users/index', {
+    users,
+    newUserPath: ctx.router.url('users-new'),
+    getShowPath: user => ctx.router.url('users-show', user.id),
+    getEditPath: user => ctx.router.url('users-edit', user.id),
+    getDestroyPath: user => ctx.router.url('users-destroy', user.id),
+  });
+});
+
+router.get('users-new', '/new', ctx => ctx.render(
+  'users/new',
+  {
+    user: ctx.orm.user.build(),
+    submitPath: ctx.router.url('users-create'),
+  },
+));
 
 router.post('users-create', '/', async (ctx) => {
   await ctx.orm.user.create(ctx.request.body);
   ctx.redirect(ctx.router.url('users'));
 });
+
 router.get('users-show', '/:id', async (ctx) => {
-  const user = await ctx.orm.user.findById(ctx.params.id);
-  ctx.assert(user, 404);
-  ctx.body = user;
-});
-router.patch('users-update', '/:id', async (ctx) => {
-  const user = await ctx.orm.user.findById(ctx.params.id);
-  ctx.assert(user, 404);
-  ctx.body = await user.update(
-    ctx.request.body,
-    { fields: ['name', 'password'] },
-  );
-});
-router.delete('users-destroy', '/:id', async (ctx) => {
-  const user = await ctx.orm.user.findById(ctx.params.id);
-  ctx.assert(user, 404);
-  await user.destroy();
-  ctx.redirect(ctx.router.url('users'));
+  ctx.body = ctx.state.user;
 });
 
+router.get('users-edit', '/:id/edit', (ctx) => {
+  const { user } = ctx.state;
+  return ctx.render(
+    'users/edit',
+    {
+      user,
+      submitPath: ctx.router.url('users-update', user.id),
+    },
+  );
+});
+
+router.patch('users-update', '/:id', async (ctx) => {
+  ctx.body = await ctx.state.user.update(
+    ctx.request.body,
+    { fields: ['name', 'email', 'phone', 'password'] },
+  );
+});
+
+router.delete('users-destroy', '/:id', async (ctx) => {
+  await ctx.state.user.destroy();
+  ctx.redirect(ctx.router.url('users'));
+});
 
 module.exports = router;
