7f27706834e31a68caafee452919c51d6aa16c59
entamburini <entamburini@uc.cl>
2018-09-22 16:34:53 -0300

    repairing routes

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -1,57 +1,37 @@
 const KoaRouter = require('koa-router');
+ const router = new KoaRouter();
 
-// Callbacks 
+ router.param('id', async (id, ctx, next) => {
+   const review = await ctx.orm.review.findById(ctx.params.id);
+   ctx.assert(review, 404);
+   ctx.state.review = review;
+   return next();
+ });
 
-async function reviewsIndexRendererCallback(ctx) {
-  const reviews = await ctx.orm.review.findAll();
-  return ctx.render('reviews/index', {
-    reviews,
-    newreviewPath: ctx.router.url('reviews-new'),
-    getShowPath: review => ctx.router.url('reviews-show', review.id),
-    getEditPath: review => ctx.router.url('reviews-edit', review.id),
-    getDestroyPath: review => ctx.router.url('reviews-destroy', review.id),
-  });
-}
+ router.get('reviews', '/', async (ctx) => {
+   const reviews = await ctx.orm.review.findAll();
+   return ctx.render('reviews/index', {
+     reviews,
+     newreviewPath: ctx.router.url('reviews-new'),
+     getShowPath: review => ctx.router.url('reviews-show', review.id),
+     getEditPath: review => ctx.router.url('reviews-edit', review.id),
+     getDestroyPath: review => ctx.router.url('reviews-destroy', review.id),
+   });
+ });
 
-async function reviewsCreateCallback(ctx) {
-  await ctx.orm.review.create(ctx.request.body);
+ router.get('reviews-new', '/new', ctx => ctx.render(
+   'reviews/new',
+   {
+     review: ctx.orm.review.build(),
+     submitPath: ctx.router.url('reviews-create'),
+   },
+ ));
+
+ router.post('reviews-create', '/', async (ctx) => {
+  await ctx.orm.review.create(ctx.review.body);
   ctx.redirect(ctx.router.url('reviews'));
-}
+ });
 
-async function reviewsShowCallback(ctx) {
-  const review = await ctx.orm.review.findById(ctx.params.id);
-  ctx.assert(review, 404);
-  ctx.body = review;
-}
-
-async function reviewsUpdateCallback(ctx) {
-  const review = await ctx.orm.review.findById(ctx.params.id);
-  ctx.assert(review, 404);
-  ctx.body = await review.update(
-    ctx.request.body,
-    { fields: ['fullfillment_offer', 'puntuality', 'quality_offered','content'] },
-  );
-}
-
-function reviewsEditRendererCallback(ctx) {
-  const { review } = ctx.state;
-  return ctx.render(
-    'reviews/edit',
-    {
-      review,
-      submitPath: ctx.router.url('reviews-update', review.id),
-    },
-  );
-}
-
-// Routes 
-
-const router = new KoaRouter();
-
-router.get('reviews', '/', reviewsIndexRendererCallback);
-router.post('reviews-create', '/', reviewsCreateCallback);
-router.patch('reviews-update', '/:id', reviewsUpdateCallback);
-router.get('reviews-edit', '/:id/edit', reviewsEditRendererCallback);
 router.get('reviews-show', '/:id', ctx => ctx.render(
   'reviews/show',
   {
@@ -60,12 +40,39 @@ router.get('reviews-show', '/:id', ctx => ctx.render(
     state: JSON.parse(JSON.stringify(ctx.state.review)),
   },
 ));
-router.get('reviews-new', '/new', ctx => ctx.render(
-  'reviews/new',
-  {
-    review: ctx.orm.review.build(),
-    submitPath: ctx.router.url('reviews-create'),
-  },
-));
 
-module.exports = router;
+ router.get('reviews-edit', '/:id/edit', (ctx) => {
+   const { review } = ctx.state;
+   return ctx.render(
+     'reviews/edit',
+     {
+       review,
+       submitPath: ctx.router.url('reviews-update', review.id),
+     },
+   );
+ });
+
+ router.patch('reviews-update', '/:id', async (ctx) => {
+   const { review } = ctx.state;
+   try {
+     await review.update(
+       ctx.review.body,
+       { fields: ['message'] },
+     );
+     ctx.redirect(ctx.router.url('reviews-show', review.id));
+   } catch (error) {
+     if (!isValidationError(error)) throw error;
+     await ctx.render('reviews/edit', {
+       review,
+       errors: getFirstErrors(error),
+       submitPath: ctx.router.url('reviews-update', review.id),
+     });
+   }
+ });
+
+ router.delete('reviews-destroy', '/:id', async (ctx) => {
+   await ctx.state.review.destroy();
+   ctx.redirect(ctx.router.url('reviews'));
+ });
+
+ module.exports = router;
