cdd77280b0c1a8bb4f31e875679ef6d91d9e76db
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-11-19 18:08:30 -0300

    :seedling: Created proof of concept

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,5 +1,6 @@
 const KoaRouter = require('koa-router');
 const Sequelize = require('sequelize');
+const vision = require('@google-cloud/vision'); 
 
 const router = new KoaRouter();
 
@@ -41,7 +42,7 @@ router.get('search', '/', async (ctx) => {
   });
 });
 
-router.get('search', '/api', async (ctx) => {
+router.get('search-api', '/api', async (ctx) => {
   const search = ctx.request.query.query;
   const publications = await ctx.orm.publication.findAll(
     {
@@ -56,5 +57,22 @@ router.get('search', '/api', async (ctx) => {
   ctx.body = publications;
 });
 
+router.get('search-vision', '/test', async (ctx) => {
+  const client = new vision.ImageAnnotatorClient({
+    keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+  });
+  client
+  .labelDetection('https://target.scene7.com/is/image/Target/GUEST_869df002-c5e0-4b9f-9636-56332268c6cf?wid=488&hei=488&fmt=pjpeg')
+  .then(results => {
+    const labels = results[0].labelAnnotations;
+
+    console.log('Labels:');
+    labels.forEach(label => console.log(label.description));
+    ctx.body = labels;
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+    });
+})
 
 module.exports = router;
