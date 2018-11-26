d1a3ba9dda5c422da2b95c63419713c724c7de2b
jatrijori <jtjorquera@uc.cl>
2018-11-26 02:31:10 -0300

    Added logo to navbar and changed home page to publication index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,42 @@
 const KoaRouter = require('koa-router');
 const Sequelize = require('sequelize');
 const pkg = require('../../package.json');
+const { forEach } = require('p-iteration');
 
 const router = new KoaRouter();
 
 router.get('/', async (ctx) => {
-  const publications = await ctx.orm.publication.findAll();
-  const pub_json = JSON.stringify(publications);
-  await ctx.render('index', {
-    appVersion: pkg.version,
-    'publications': pub_json,
-  });
+	if (ctx.state.currentUser) {
+		const publications = await ctx.orm.publication.findAll();
+    let users_names = {};
+    let items_ids = {};
+    await forEach(publications, async (pub) => {
+      const user = await pub.getUser();
+      users_names[pub.id] = user.name;
+    });
+    await forEach(publications, async (pub) => {
+      const item = await pub.getItem();
+      items_ids[pub.id] = item.id;
+      pub.dataValues['item'] = item;
+    });
+    return ctx.render('publications/index', {
+      publications,
+      users_names,
+      items_ids,
+      getItemImagePath: item_id => ctx.router.url('items-show-image', item_id),
+      newpublicationPath: ctx.router.url('publications-new'),
+      getShowPath: publication => ctx.router.url('publications-show', publication.id),
+      getEditPath: publication => ctx.router.url('publications-edit', publication.id),
+      getDestroyPath: publication => ctx.router.url('publications-destroy', publication.id),
+    });
+	} else {
+		const publications = await ctx.orm.publication.findAll();
+  	const pub_json = JSON.stringify(publications);
+  	await ctx.render('index', {
+    	appVersion: pkg.version,
+    	'publications': pub_json,
+	  });
+	}
 });
 
 module.exports = router;
