55e6cd1595bfdf41fc043f3f23b0603c04d794e9
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-11-27 19:10:20 -0300

    :wrench: config babel to admit special imports

--- babel.config.js
+++ babel.config.js
@@ -16,6 +16,7 @@ module.exports = function babelConfig(api) {
 
   const plugins = [
     'react-hot-loader/babel',
+    '@babel/plugin-syntax-dynamic-import',
   ];
   return { presets, plugins };
 };
