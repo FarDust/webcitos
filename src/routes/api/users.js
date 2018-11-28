0b349f57a03fde3ef5b31ee30eec2066f06a7400
jatrijori <jtjorquera@uc.cl>
2018-11-28 11:53:39 -0300

    Added get publications of token user to api

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,14 +1,34 @@
 const KoaRouter = require('koa-router');
 const jwtSign = require('jsonwebtoken').sign;
+const _ = require('lodash');
 
 const router = new KoaRouter();
+
+function apiUrl(ctx, ...params) {
+	return `${ctx.origin}${ctx.router.url(...params)}`;
+}
 
 router.get('users-me', '/me', async (ctx) => {
   ctx.assert(ctx.state.jwtdata, 401);
   const user = await ctx.orm.user.findById(ctx.state.jwtdata.userId);
   ctx.assert(ctx.state.jwtdata, 403);
   ctx.body = user;
- });
+});
+
+router.get('users-my-publications', '/my-publications', async (ctx) => {
+  ctx.assert(ctx.state.jwtdata, 401);
+  const user = await ctx.orm.user.findById(ctx.state.jwtdata.userId);
+  ctx.assert(ctx.state.jwtdata, 403);
+  const publications = await user.getPublications();
+  const publicationsJSON = publications.map(publi => ({
+    ..._.pick(publi, 'id', 'title', 'description', 'state'),
+    links: {
+    	self: apiUrl(ctx, 'publications-show', publi.id),
+      //item: apiUrl(ctx, 'items-show', publi.id),
+    },
+  }));
+  ctx.body = publicationsJSON;
+});
 
 router.put('users-session', '/session', async (ctx) => {
   const { email, password } = ctx.request.body;
